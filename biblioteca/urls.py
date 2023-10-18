866e55822855338d77e61c7f3636ca6d415547d7
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-18 14:16:53 +0200

    Urls y Views

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('libros/listar',views.listar_libros, name='listar_libros'),
+    path('libros/<int:id_libro>', views.dame_libro, name="dame_libro"),
 ]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Libro
 # Create your views here.
 def index(request):
     return render(request,'biblioteca/index.html')
+
+def listar_libros(request):
+    libros = Libro.objects.select_related("biblioteca").prefetch_related("autores")
+    return render(request,'libro/lista.html',{"libros_mostrar":libros})
+
+def dame_libro(request,id_libro):
+    libro = Libro.objects.select_related("biblioteca").prefetch_related("autores").get(id = id_libro)
+    return render(request,'libro/libro.html',{"libro_mostrar":libro})
