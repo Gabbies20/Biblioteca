67ea7f5008832709b5a4bece0e2e6ffccf25ea61
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-02 10:57:25 +0100

    Aprendiendo Include de templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i-37vu1%yv1gm1#aw%+d4h6a3hcp7)x_u-9#ktq7a45ug1*+&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0']
 
